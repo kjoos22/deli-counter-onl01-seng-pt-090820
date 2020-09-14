8ece6ef73987bd56988aaf3800574bc837b9be3a
Kristian Joos <kjoos22@gmail.com>
2020-09-14 21:26:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |customer|
-      line += " " + String(x) + ". #{customer}"
+      line += " #{String(x)}. #{customer}"
       x += 1
     end
     puts line
